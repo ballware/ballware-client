7e32ba3c7944c6700bffa863ba9d06a3fed9cb88
Frank Ballmeyer <Frank.Ballmeyer@assecosol.com>
2021-04-07 18:07:21 +0200

    Webclient added import/export functions

--- apps/webclient/config-overrides.js
+++ apps/webclient/config-overrides.js
@@ -1,7 +1,11 @@
 const fs = require ('fs');
-const { override, babelInclude } = require("customize-cra");
+const path = require('path');
+const { override, babelInclude, removeModuleScopePlugin } = require("customize-cra");
+const {aliasDangerous, configPaths} = require('react-app-rewire-alias/lib/aliasDangerous');
+
 module.exports = function (config, env) {
   return Object.assign(config, override(
+    aliasDangerous({...configPaths('tsconfig.paths.json')}),
     babelInclude([
       fs.realpathSync('src'),
       fs.realpathSync('../../packages')
