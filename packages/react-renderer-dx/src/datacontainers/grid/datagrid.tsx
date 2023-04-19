480b6ea80fb22bd5202b46d3b6e1eba724ba9cfe
Frank Ballmeyer <frank@ballmeyer.com>
2023-04-19 17:18:27 +0200

    Datagrid: Add select/unselect all rows including visible warning

--- packages/react-renderer-dx/src/datacontainers/grid/datagrid.tsx
+++ packages/react-renderer-dx/src/datacontainers/grid/datagrid.tsx
@@ -5,7 +5,7 @@
  * SPDX-License-Identifier: MIT
  */
 
-import React, { useMemo, useContext, useRef, useEffect } from 'react';
+import React, { useMemo, useContext, useRef, useEffect, useCallback, useState } from 'react';
 
 import {
   DataGrid as DxDataGrid,
@@ -18,10 +18,10 @@ import {
   Selection,
   MasterDetail,
   ColumnChooser,
-  Paging,
+  Paging
 } from 'devextreme-react/data-grid';
 import DataSource from 'devextreme/data/data_source';
-import dxDataGrid, { dxDataGridColumn, dxDataGridRowObject, EditingStartEvent, RowKeyInfo } from 'devextreme/ui/data_grid';
+import dxDataGrid, { Toolbar, dxDataGridColumn, dxDataGridRowObject, EditingStartEvent, RowKeyInfo } from 'devextreme/ui/data_grid';
 import {
   CrudItem,
   GridLayout,
@@ -34,6 +34,8 @@ import { dxElement } from 'devextreme/core/element';
 import { dxToolbarItem, dxToolbarOptions } from 'devextreme/ui/toolbar';
 import { MetaContext, CrudContext, EditModes } from '@ballware/react-contexts';
 import { useTranslation } from 'react-i18next';
+import { ToolbarItem } from 'devextreme/ui/data_grid';
+import dxButton from 'devextreme/ui/button';
 
 export interface DataGridProps {
   height: string;
@@ -105,6 +107,50 @@ export const DataGrid = ({
   const selectedRowData = useRef<Array<CrudItem>>([] as CrudItem[]);
 
   const grid = useRef<DxDataGrid>(null);
+  const gridSelection = useRef<dxButton|undefined>(undefined);
+  
+  const onVisibleRowsChanged = useCallback((visibleKeys: string[]) => {
+    if (gridSelection.current) {
+      const invisibleKeys = selectedRowKeys.current.filter(sr => !visibleKeys.includes(sr));
+
+      if (invisibleKeys.length && gridSelection.current) {
+        gridSelection.current.option('hint', t('datacontainer.actions.selectionhint.danger'));
+        gridSelection.current.option('type', 'danger');
+        gridSelection.current.option('icon', 'bi bi-exclamation-triangle');
+      } else {
+        gridSelection.current.option('hint', t('datacontainer.actions.selectionhint.normal'));
+        gridSelection.current.option('type', 'normal');
+        gridSelection.current.option('icon', undefined);
+      }
+    }
+  }, [t, gridSelection]);
+
+  const onSelectAllClick = useCallback(() => {
+    grid.current?.instance.selectAll();
+
+    onVisibleRowsChanged(grid.current?.instance.getVisibleRows().map(vr => vr.key) ?? []);
+  }, []);
+
+  const onUnselectAllClick = useCallback(() => {
+    grid.current?.instance.clearSelection();
+
+    onVisibleRowsChanged(grid.current?.instance.getVisibleRows().map(vr => vr.key) ?? []);
+  }, []);
+
+  const onNavigateToSelection = useCallback(() => {
+    if (selectedRowKeys.current.length) {
+      grid.current?.instance.navigateToRow(selectedRowKeys.current[0]);
+    }
+  }, []);
+
+  const onSelectionChanged = useCallback((selectedKeys: string[]) => {
+    if (gridSelection.current) {
+      gridSelection.current.option('visible', selectedKeys.length > 0);
+      gridSelection.current.option('text', t('datacontainer.actions.selection', { count: selectedKeys.length }));
+    }
+
+    onVisibleRowsChanged(grid.current?.instance.getVisibleRows().map(vr => vr.key) ?? []);
+  }, [t, gridSelection]);
 
   useEffect(() => {
     if (isLoading) {
@@ -117,90 +163,100 @@ export const DataGrid = ({
   }, [isLoading, t]);
 
   return useMemo(() => {
+
+    if (mode !== 'large') {
+      selectedRowKeys.current = [];
+      selectedRowData.current = [];
+    }
+
     const onCustomizeColumnsDisableHidingPriority = (columns: Array<any>) => {
       columns?.forEach(c => (c.hidingPriority = null));
     };
 
-    const onToolbarPreparing = (e: { toolbarOptions?: dxToolbarOptions }) => {
-      if (showReload && e.toolbarOptions?.items) {
-        e.toolbarOptions.items.unshift({
-          locateInMenu: 'auto',
-          location: 'after',
-          widget: 'dxButton',
-          showText: 'inMenu',
-          options: {
-            hint: t('datacontainer.actions.refresh'),
-            text: t('datacontainer.actions.refresh'),
-            icon: 'bi bi-arrow-repeat',
-            onClick: (e: { event: { currentTarget: Element } }) => {
-              onReloadClick({ target: e.event.currentTarget });
-            },
-          },
-        } as dxToolbarItem);
-      }
+    const toolbar = {
+      items: []
+    } as Toolbar;
 
-      if (showPrint && e.toolbarOptions?.items) {
-        e.toolbarOptions.items.unshift({
-          locateInMenu: 'auto',
-          location: 'after',
-          widget: 'dxButton',
-          showText: 'inMenu',
-          options: {
-            hint: t('datacontainer.actions.print'),
-            text: t('datacontainer.actions.print'),
-            icon: 'bi bi-printer-fill',
-            onClick: (e: { event: { currentTarget: Element } }) => {
-              onPrintClick({
-                items: selectedRowData.current,
-                target: e.event.currentTarget,
-              });
-            },
+    if (showReload) {
+      toolbar.items?.unshift({
+        locateInMenu: 'auto',
+        location: 'after',
+        widget: 'dxButton',
+        showText: 'inMenu',
+        options: {
+          hint: t('datacontainer.actions.refresh'),
+          text: t('datacontainer.actions.refresh'),
+          icon: 'bi bi-arrow-repeat',
+          onClick: (e: { event: { currentTarget: Element } }) => {
+            onReloadClick({ target: e.event.currentTarget });
           },
-        });
-      }
+        },
+      } as dxToolbarItem);
+    }
 
-      if (showImport && e.toolbarOptions?.items) {
-        e.toolbarOptions.items.unshift({
-          locateInMenu: 'auto',
-          location: 'after',
-          widget: 'dxButton',
-          showText: 'inMenu',
-          options: {
-            hint: t('datacontainer.actions.import'),
-            text: t('datacontainer.actions.import'),
-            icon: 'bi bi-upload',
-            onClick: (e: { event: { currentTarget: Element } }) => {
-              onImportClick({
-                items: selectedRowData.current,
-                target: e.event.currentTarget,
-              });
-            },
+    if (showPrint) {
+      toolbar.items?.unshift({
+        locateInMenu: 'auto',
+        location: 'after',
+        widget: 'dxButton',
+        showText: 'inMenu',
+        options: {
+          hint: t('datacontainer.actions.print'),
+          text: t('datacontainer.actions.print'),
+          icon: 'bi bi-printer-fill',
+          onClick: (e: { event: { currentTarget: Element } }) => {
+            onPrintClick({
+              items: selectedRowData.current,
+              target: e.event.currentTarget,
+            });
           },
-        });
-      }
+        },
+      });
+    }
 
-      if (showExport && e.toolbarOptions?.items) {
-        e.toolbarOptions.items.unshift({
-          locateInMenu: 'auto',
-          location: 'after',
-          widget: 'dxButton',
-          showText: 'inMenu',
-          options: {
-            hint: t('datacontainer.actions.export'),
-            text: t('datacontainer.actions.export'),
-            icon: 'bi bi-download',
-            onClick: (e: { event: { currentTarget: Element } }) => {
-              onExportClick({
-                items: selectedRowData.current,
-                target: e.event.currentTarget,
-              });
-            },
+    if (showImport) {
+      toolbar.items?.unshift({
+        locateInMenu: 'auto',
+        location: 'after',
+        widget: 'dxButton',
+        showText: 'inMenu',
+        options: {
+          hint: t('datacontainer.actions.import'),
+          text: t('datacontainer.actions.import'),
+          icon: 'bi bi-upload',
+          onClick: (e: { event: { currentTarget: Element } }) => {
+            onImportClick({
+              items: selectedRowData.current,
+              target: e.event.currentTarget,
+            });
           },
-        });
-      }
+        },
+      });
+    }
 
+    if (showExport) {
+      toolbar.items?.unshift({
+        locateInMenu: 'auto',
+        location: 'after',
+        widget: 'dxButton',
+        showText: 'inMenu',
+        options: {
+          hint: t('datacontainer.actions.export'),
+          text: t('datacontainer.actions.export'),
+          icon: 'bi bi-download',
+          onClick: (e: { event: { currentTarget: Element } }) => {
+            onExportClick({
+              items: selectedRowData.current,
+              target: e.event.currentTarget,
+            });
+          },
+        },
+      });
+    }
+
+    if (mode === 'large' && layout.allowMultiselect) {
       customFunctions?.forEach(f => {
-        e.toolbarOptions?.items?.unshift({
+        toolbar.items?.unshift({
           locateInMenu: 'auto',
           location: 'after',
           widget: 'dxButton',
@@ -219,24 +275,86 @@ export const DataGrid = ({
           },
         } as dxToolbarItem);
       });
+    }    
 
-      if (showAdd) {
-        e.toolbarOptions?.items?.unshift({
-          locateInMenu: 'auto',
-          location: 'after',
-          widget: 'dxButton',
-          showText: 'inMenu',
-          options: {
-            hint: t('datacontainer.actions.add', { entity: displayName }),
-            text: t('datacontainer.actions.add', { entity: displayName }),
-            icon: 'bi bi-plus',
-            onClick: (e: { event: { currentTarget: Element } }) => {
-              onAddClick({ target: e.event.currentTarget });
-            },
+    if (showAdd) {
+      toolbar.items?.unshift({
+        locateInMenu: 'auto',
+        location: 'after',
+        widget: 'dxButton',
+        showText: 'inMenu',
+        options: {
+          hint: t('datacontainer.actions.add', { entity: displayName }),
+          text: t('datacontainer.actions.add', { entity: displayName }),
+          icon: 'bi bi-plus',
+          onClick: (e: { event: { currentTarget: Element } }) => {
+            onAddClick({ target: e.event.currentTarget });
           },
-        });
-      }
-    };
+        },
+      });
+    }
+
+    if (mode === 'large' && layout.allowMultiselect) {
+      toolbar.items?.unshift({
+        locateInMenu: 'auto',
+        location: 'after',
+        widget: 'dxButton',
+        showText: 'inMenu',
+        options: {
+          hint: t('datacontainer.actions.unselectall', {}),
+          text: t('datacontainer.actions.unselectall', { count: 99 }),
+          icon: 'bi bi-dash-square',
+          onClick: () => {
+            onUnselectAllClick();
+          },
+        }
+      } as ToolbarItem);
+
+      toolbar.items?.unshift({
+        locateInMenu: 'never',
+        location: 'after',
+        widget: 'dxButton',
+        showText: 'always',
+        options: {
+          visible: selectedRowKeys.current.length > 0,
+          hint: t('datacontainer.actions.selectionhint.normal'),
+          text: t('datacontainer.actions.selection', { count: selectedRowKeys.current.length }),
+          onClick: () => {
+            onNavigateToSelection();
+          },
+          onInitialized: (e: { component: dxButton }) => {
+            gridSelection.current = e.component;
+          }
+        }
+      } as ToolbarItem);
+
+      toolbar.items?.unshift({
+        locateInMenu: 'auto',
+        location: 'after',
+        widget: 'dxButton',
+        showText: 'inMenu',
+        options: {
+          hint: t('datacontainer.actions.selectall', {}),
+          text: t('datacontainer.actions.selectall', {}),
+          icon: 'bi bi-check-square',
+          onClick: () => {
+            onSelectAllClick();
+          },
+        }
+      } as ToolbarItem)
+    }
+
+    toolbar.items?.push('exportButton');
+
+    if (mode === 'large') {
+      toolbar.items?.push('columnChooserButton');
+    }
+
+    toolbar.items?.push('searchPanel');
+
+    if (mode === 'large') {
+      toolbar.items?.unshift('groupPanel');
+    }
 
     const onEditingStart = (e: EditingStartEvent) => {
       e.cancel = !e.data || !editAllowed || !editAllowed(e.data);
@@ -334,14 +452,17 @@ export const DataGrid = ({
         remoteOperations={{ sorting: false, filtering: false }}
         columnAutoWidth={true}
         rowAlternationEnabled
+        onContentReady={e => onVisibleRowsChanged(e.component.getVisibleRows()?.map(r => r.key) ?? [])}
         onEditingStart={onEditingStart}
         onEditorPreparing={gridEditorPreparing}
         onSelectionChanged={e => {
           selectedRowKeys.current = e.selectedRowKeys as string[];
           selectedRowData.current = e.selectedRowsData as CrudItem[];
+
+          onSelectionChanged(selectedRowKeys.current);
         }}
-        onToolbarPreparing={onToolbarPreparing}
         onRowExpanding={onRowExpanding}
+        toolbar={toolbar}
         customizeColumns={
           mode === 'large' ? onCustomizeColumnsDisableHidingPriority : undefined
         }
